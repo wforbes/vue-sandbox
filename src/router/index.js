16c372a0a3c6c1c857c42cd1df6c9e58ddf8a6d8
Will Forbes <wforbes87@gmail.com>
2022-01-02 00:25:44 -0800

    restructure boilerplate and directories

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+	{
+		path: "/",
+		name: "HomePage",
+		component: () =>
+			import(/* webpackChunkName: "home" */ "@/home/views/HomePage.vue")
+	},
+	{
+		path: "/about",
+		name: "AboutPage",
+		component: () =>
+			import(/* webpackChunkName: "home" */ "@/home/views/AboutPage.vue")
+	}
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes
 });
 
 export default router;
