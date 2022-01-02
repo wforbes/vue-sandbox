716647a38119843e3a96924981102b847b65c0bc
Will Forbes <wforbes87@gmail.com>
2022-01-02 00:23:21 -0800

    add my preferred linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+	root: true,
+	env: {
+	  node: true,
+	},
+	extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+	parserOptions: {
+	  parser: "babel-eslint",
+	},
+	rules: {
+	  "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+	  "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+	  "no-redeclare": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-async-promise-executor": "off",
+		indent: ["error", "tab"],
+	  "prettier/prettier": [
+		"warn",
+		{
+		  useTabs: true,
+		  trailingComma: "none",
+		  endOfLine: "auto"
+		}
+	  ]
+	},
+  };
+  
